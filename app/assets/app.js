7d8838f1f5d155116d3820c2b9acf2069a21795c
jbiard <1724744+jonathanbiard@users.noreply.github.com>
2024-02-19 00:57:04 -0500

    Added most of the form for the frontend with most of the styling

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
-import Home from './controllers/Home.vue'
+import App from './controllers/App.vue'
 
-import './styles/base.scss'
-import './styles/Home.scss'
+import './styles/app.scss'
 
 const minimumValue = 1
 const maximumValue = 100
 
-createApp(Home, {
+createApp(App, {
     number: Math.floor(Math.random() * (maximumValue - minimumValue + 1) + minimumValue)
 }).mount('#app')
